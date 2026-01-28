8509b1d4a3a3399f0c8ba8ba53fd292b41c20020
AlaryVanEeckhout <139067376+AlaryVanEeckhout@users.noreply.github.com>
2026-01-28 15:43:24 -0500

    Margin fix, title newline test

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Le comité pro-énergie nucléaire";
+export const SITE_TITLE_0 = "Le comité";
+export const SITE_TITLE_1 = "pro-énergie nucléaire";
 export const SITE_DESCRIPTION = "Welcome to my website!";
